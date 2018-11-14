a48627b78981ab86369e39c142cffb4c5f6d98be
Thibault Clérice <leponteineptique@gmail.com>
2018-11-14 18:00:31 +0100

    Removed word count that can create an overhead

--- cmds/tei.sh
+++ cmds/tei.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-hooktest --scheme tei --console --verbose 10 --json /sources/$1/result.json -w 5 --guidelines 2.tei --countword /sources/$1
+hooktest --scheme tei --console --verbose 10 --json /sources/$1/result.json -w 5 --guidelines 2.tei /sources/$1
 echo "data = "|cat - /sources/$1/result.json > /tmp/out && mv /tmp/out /sources/$1/result.json
 cp /cmds/results.html /sources/$1/results.html
